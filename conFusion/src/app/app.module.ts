9564dbf7538ac1e79b360a2edf70b1847a5357da
surakhchin <urakhchin@gmail.com>
2020-12-14 20:37:09 +0300

    added mat design buttons and grid layout to menu page and app module

--- conFusion/src/app/app.module.ts
+++ conFusion/src/app/app.module.ts
@@ -5,6 +5,9 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { MatListModule } from '@angular/material/list';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 
 // hammerjs is used for gestures
 import 'hammerjs';
@@ -23,6 +26,9 @@ import { MenuComponent } from './menu/menu.component';
     MatToolbarModule,
     FlexLayoutModule,
     MatListModule,
+    MatGridListModule,
+    MatCardModule,
+    MatButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
